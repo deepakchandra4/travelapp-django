0c0babee55556a8fb93b0831c41db9d523e0155a
Deepak Chandra Maurya <deepakchandra4551@gmail.com>
2025-08-30 03:53:28 +0530

    All thing working

--- travelapp/settings.py
+++ travelapp/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = "travelapp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "travelapp_db",
+        "NAME": "travelappdb",
         "USER": "root",
         "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "3306",
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
         },
     }
 }
